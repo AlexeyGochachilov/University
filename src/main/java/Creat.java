1f3f44e3b159a1f58a7a5c2082cd0047e6cd7e73
AlexeyGochachilov <agochachilov@gmail.com>
2025-03-24 10:54:55 +0300

    first step

---
+++ src/main/java/Creat.java
@@ -0,0 +1,14 @@
+public class Creat {
+
+    public static void main(String[] args) {
+
+        University mgu = new University("01", "Московский государственный университет",
+                "МГУ", 1755, StudyProfile.ECONOMICS);
+
+        Student ivanov = new Student(
+                4.5f, 3, "01", "Иванов Иван Иванович");
+
+        System.out.println(mgu);
+        System.out.println(ivanov);
+    }
+}
